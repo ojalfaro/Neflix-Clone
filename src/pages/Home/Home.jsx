1a962a64f5b0ade57ef60c3e8f7868ecb96156d0
Javier Alfaro <ojalfarob@gmail.com>
2024-10-29 16:21:14 -0600

    add banner at home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
 const home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos amet
+            voluptates nihil sit inventore dolor, fugit tempore voluptatum
+            possimus neque, mollitia qui labore dignissimos repellendus?
+            Temporibus ipsum nulla cupiditate saepe! Sit, non quaerat magnam
+            odio ipsum laboriosam autem aspernatur ipsam pariatur voluptate
+            dicta nostrum corrupti, illum velit animi repudiandae ipsa
+            cupiditate ea perspiciatis minus! Qui necessitatibus eaque quis
+            tenetur nulla!
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
