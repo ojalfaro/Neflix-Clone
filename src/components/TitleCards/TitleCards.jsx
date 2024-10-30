45ff1b7f94175b0cfdc4e1f0b7cd6b9cf059ad6b
Javier Alfaro <ojalfarob@gmail.com>
2024-10-30 08:02:15 -0600

    add titlecards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
-  return <div className="titlecards"></div>;
+  const cardsref = useRef();
+  const handleWeel = (event) => {
+    event.preventDefault();
+    cardsref.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsref.current.addEventListener("wheel", handleWeel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsref}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const home = () => {
   return (
@@ -34,6 +35,7 @@ const home = () => {
               More info
             </button>
           </div>
+          <TitleCards />
         </div>
       </div>
     </div>
